b9f464b3bc7429351c7028179e32bf616ebd7c3c
MrSoupman <ioxflash@gmail.com>
2021-11-10 02:46:31 -0500

    Created GetSHA method

--- TinySync/ViewModel/ShaSvc.cs
+++ TinySync/ViewModel/ShaSvc.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Security.Cryptography;
+using System.IO;
 
 namespace TinySync.ViewModel
 {
-    class ShaSvc
+    public class ShaSvc
     {
+        public static string GetSHA(string file)
+        {
+            using (SHA256 hasher = SHA256.Create())
+            {
+                try
+                {
+                    FileStream stream = File.Open(file, FileMode.Open);
+                    stream.Position = 0;
+                    byte[] hash = hasher.ComputeHash(stream);
+                    stream.Close();
+                    string sha = "";
+                    for (int i = 0; i < hash.Length; i++)
+                    {
+                        sha += $"{hash[i]:X2}";
+                    }                    
+                    return sha;
+                }
+                catch (IOException e)
+                {
+                    throw;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
